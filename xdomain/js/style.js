1bc8605080c6a74bdc49767a96a6040c4b109cce
Wasapon7763 <wasapon7763@gmail.com>
2016-08-03 14:22:56 +0900

    8/3

--- xdomain/js/style.js
+++ xdomain/js/style.js
@@ -1,24 +1,7 @@
-$(function () {
-    sizing();
-    clearInterval(interval);
-    init();
-    $(window).resize(function() {
-        sizing();
-        clearInterval(interval);
-        init();
-    });
-});
 $(function(){
-    $("#info").click(function(){
+    $("#open").click(function(){
 				$(this).toggleClass("active");
         $("#sidebar").toggleClass("active");
         return false;
     });
 });
-function sizing(){
-    $('#cnvs').attr({height:$('#wrapper').height()});
-    $('#cnvs').attr({width:$('#wrapper').width()});
-};
-function showTriangle(i){
-  var $=document.getElementsByName("about");
-}
